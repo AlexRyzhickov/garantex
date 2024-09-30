32f60ceeaddb8676363b87a0c0123df3ba9db1ec
AlexRyzhickov <makseljoinb@gmail.com>
2024-10-01 00:03:30 +0300

    updated repo

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -22,12 +22,14 @@ func BuildUpsertQuery(table string, price models.Price) string {
 	buf = append(buf, "INSERT INTO "...)
 	buf = append(buf, table...)
 	buf = append(buf, " (ts, ask_price, bid_price) VALUES "...)
+	buf = append(buf, "("...)
+	buf = append(buf, strconv.Itoa(int(price.Timestamp))...)
+	buf = append(buf, ',')
 	buf = strconv.AppendFloat(buf, price.AskPrice, 'f', -1, 32)
 	buf = append(buf, ',')
 	buf = strconv.AppendFloat(buf, price.BidPrice, 'f', -1, 32)
-	buf = append(buf, ',')
-	buf = append(buf, strconv.Itoa(int(price.Timestamp))...)
-	buf = append(buf, " ON CONFLICT (id) DO UPDATE SET ts = EXCLUDED.ts, ask_price = EXCLUDED.ask_price, bid_price = EXCLUDED.bid_price;"...)
+	buf = append(buf, ")"...)
+	buf = append(buf, " ON CONFLICT (ts) DO UPDATE SET ts = EXCLUDED.ts, ask_price = EXCLUDED.ask_price, bid_price = EXCLUDED.bid_price;"...)
 	return string(buf)
 }
 
